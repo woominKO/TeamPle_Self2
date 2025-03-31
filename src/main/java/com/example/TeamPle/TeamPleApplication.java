5d44634f12fee3beb319ec7c610978d51b8865d2
woominKO <woomin422@gmail.com>
2025-03-31 15:34:41 +0900

    init

---
+++ src/main/java/com/example/TeamPle/TeamPleApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamPle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPleApplication.class, args);
+	}
+
+}
